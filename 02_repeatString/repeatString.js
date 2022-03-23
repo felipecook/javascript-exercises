836623ddfee10ebd605bec488e33e8312d19d41f
felipe cook <felipepcook@gmail.com>
2022-03-23 11:10:52 -0400

    solution using recursion

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else if(num == 0) {
+    return '';
+  } else if (num == 1){
+    return string;
+  } else {
+    return string + repeatString(string, num - 1);
+  }
 
 };
 
