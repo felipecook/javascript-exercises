1480535c53b96ac06052c9bd82c894657dc2958d
felipe cook <felipepcook@gmail.com>
2022-03-23 16:13:49 -0400

    solution using loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (fullArray, ...remove) {
+  for (let i = 0; i < remove.length; i++){
+    for(let j = 0; j < fullArray.length; j++){
+      if (remove[i] == fullArray[j]){
+        if (Number.isInteger(remove[i])){
+          fullArray.splice(j, 1);
+        }
+      }
+    }
+  }
+  return fullArray;
 
 };
 
